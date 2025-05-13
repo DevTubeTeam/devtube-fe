53645943f8913b3cd0b1d1b0eaccc5725e9975df
Võ Phú Vinh <52100947@student.tdtu.edu.vn>
2025-05-13 09:56:50 +0700

    commit

--- src/utils/redirectToGoogleSilentLogin.ts
+++ src/utils/redirectToGoogleSilentLogin.ts
@@ -1,5 +1,5 @@
 const redirectToGoogleSilentLogin = () => {
-  const redirectUri = encodeURIComponent('http://localhost:3000/auth/silent/callback');
+  const redirectUri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI!);
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID!;
   const googleOAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid%20email%20profile&prompt=none`;
 
