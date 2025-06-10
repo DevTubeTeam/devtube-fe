e166d9057f2f92d7cd7bc494be5b0460dbba3794
Võ Phú Vinh <52100947@student.tdtu.edu.vn>
2025-06-11 00:37:11 +0700

    commit

--- src/utils/redirectToGoogleSilentLogin.ts
+++ src/utils/redirectToGoogleSilentLogin.ts
@@ -1,11 +1,11 @@
 const redirectToGoogleSilentLogin = () => {
   console.log('Redirecting to Google OAuth for silent login...');
-  if (!import.meta.env.VITE_REDIRECT_URI || !import.meta.env.VITE_GOOGLE_CLIENT_ID) {
+  if (!import.meta.env.VITE_GOOGLE_REDIRECT_URI || !import.meta.env.VITE_GOOGLE_CLIENT_ID) {
     console.error('Environment variables for Google OAuth are not set.');
     return;
   }
 
-  const redirectUri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI!);
+  const redirectUri = encodeURIComponent(import.meta.env.VITE_GOOGLE_REDIRECT_URI!);
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID!;
   const googleOAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid%20email%20profile&prompt=none`;
 
